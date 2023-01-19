fffcbdac1c83cebb49329bc977caeabab094c5ee
emmahewson <emhewson@hotmail.com>
2023-01-19 16:14:37 +0000

    add updated google map code

--- 01-GoogleMaps/01-adding_the_map_element/assets/js/maps.js
+++ 01-GoogleMaps/01-adding_the_map_element/assets/js/maps.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 5,
         center: {
             lat: 40.967175642407305,
@@ -7,20 +7,30 @@ function initMap() {
         }
     });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
+    const locations = [
+    { lat: 40.785091, lng: -73.968285 },
+    { lat: 41.084045, lng: -73.874245 },
+    { lat: 40.754932, lng: -73.984016 }
+];
 
-    var markers = locations.map(function (location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+    const markers = locations.map((position, i) => {
+        const label = labels[i % labels.length];
+        const marker = new google.maps.Marker({
+          position,
+          label,
         });
-    });
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    
+        marker.addListener("click", () => {
+          infoWindow.setContent(label);
+          infoWindow.open(map, marker);
+        });
+        return marker;
+      });
+
+      const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
 
 };
+
+
--- 01-GoogleMaps/06-moving_the_code_into_its_own_script/assets/js/maps.js
+++ 01-GoogleMaps/06-moving_the_code_into_its_own_script/assets/js/maps.js
@@ -23,4 +23,4 @@ function initMap() {
     });
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+};
