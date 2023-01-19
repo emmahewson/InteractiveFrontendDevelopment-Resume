642968a0ca49ef99ffdb6db2a8d1baa588d81e1f
emmahewson <emhewson@hotmail.com>
2023-01-19 15:35:08 +0000

    add Google Map

---
+++ 01-GoogleMaps/01-adding_the_map_element/assets/js/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: {
+            lat: 40.967175642407305,
+            lng: -73.90274815959594
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+};
